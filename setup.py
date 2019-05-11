21814c1aa7f9e355f1849a610aa738db7df5727c
VainF <2218880241@qq.com>
2019-05-11 15:48:48 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytorch_msssim",
+    version="0.1.0",
+    author="Gongfan Fang",
+    author_email="fgfvain97@zju.edu.cn",
+    description="A fast and differentiable MS-SSIM and SSIM for pytorch.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/VainF/pytorch-msssim",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['torch']
+)
