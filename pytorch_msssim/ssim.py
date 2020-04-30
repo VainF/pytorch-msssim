1e5eb8127696ab692de6332bac65744293320eb5
VainF <2218880241@qq.com>
2020-04-30 17:29:35 +0800

    Default data range: 255

--- pytorch_msssim/ssim.py
+++ pytorch_msssim/ssim.py
@@ -218,7 +218,7 @@ def ms_ssim(X, Y,
 
 class SSIM(torch.nn.Module):
     def __init__(self, 
-                 data_range=None, 
+                 data_range=255, 
                  size_average=True, 
                  win_size=11, 
                  win_sigma=1.5, 
@@ -255,7 +255,7 @@ class SSIM(torch.nn.Module):
 
 class MS_SSIM(torch.nn.Module):
     def __init__(self, 
-                 data_range=None, 
+                 data_range=255, 
                  size_average=True, 
                  win_size=11, 
                  win_sigma=1.5, 
